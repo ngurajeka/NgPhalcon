f9b0d457a994d60975da2e735e5b77c49afeb784
Ady Rahmat MA <ngurajeka.92@gmail.com>
2016-06-21 19:16:06 +0700

    Implement Magic Method
    using __call on NgModel and implement prefix.

--- src/Ng/Phalcon/Model/NgModel.php
+++ src/Ng/Phalcon/Model/NgModel.php
@@ -2,6 +2,7 @@
 namespace Ng\Phalcon\Model;
 
 
+use Ng\Module\Constants\Error\Error;
 use Phalcon\Mvc\Model;
 
 abstract class NgModel extends Model
@@ -10,6 +11,62 @@ abstract class NgModel extends Model
     const DELETED   = 1;
     const ID        = "id";
 
+    public function __call($method, $arguments) {
+
+        $key    = substr($method, 0, 3);
+        $field  = lcfirst(substr($method, 3));
+
+        $return = $this;
+
+        if (!in_array($field, get_object_vars($this))) {
+            throw new Model\Exception(Error::notFound("Property"));
+        }
+
+        switch ($key) {
+            case "get":
+                $return         = $this->{$field};
+                break;
+            case "set":
+                $this->{$field} = $arguments[0];
+                break;
+        }
+
+        return $return;
+    }
+
+    protected function implementSoftDelete()
+    {
+        return true;
+    }
+
+    protected static function usePrefix()
+    {
+        return "";
+    }
+
+    protected function ifExist($key, $default, $translate=true)
+    {
+        if ($translate) {
+            $key = self::translateKey($key);
+        }
+
+        $properties = get_object_vars($this);
+        if (in_array($key, $properties)) {
+            return $this->{$key};
+        }
+
+        return $default;
+    }
+
+    protected static function translateKey($key)
+    {
+        if (!empty(self::usePrefix()) AND is_string(self::usePrefix())) {
+            $key = sprintf("%s%s", self::usePrefix(), ucfirst($key));
+        }
+
+        return $key;
+    }
+
     /**
      * Get Id from Model
      *
@@ -17,7 +74,7 @@ abstract class NgModel extends Model
      */
     public function getId()
     {
-        return 0;
+        return $this->ifExist(self::ID, 0);
     }
 
     /**
@@ -27,7 +84,7 @@ abstract class NgModel extends Model
      */
     public static function getCreatedFields()
     {
-        return array();
+        return array("at" => self::translateKey("createdTime"));
     }
 
     /**
@@ -37,7 +94,10 @@ abstract class NgModel extends Model
      */
     public static function getDeletedFields()
     {
-        return array();
+        return array(
+            "deleted"   => self::translateKey("deleted"),
+            "at"        => self::translateKey("deletedTime"),
+        );
     }
 
     /**
@@ -57,7 +117,7 @@ abstract class NgModel extends Model
      */
     public static function getPrimaryKey()
     {
-        return self::ID;
+        return self::translateKey(self::ID);
     }
 
     /**
@@ -67,7 +127,7 @@ abstract class NgModel extends Model
      */
     public static function getPublicFields()
     {
-        return array();
+        return array(self::translateKey(self::ID));
     }
 
     /**
@@ -87,7 +147,7 @@ abstract class NgModel extends Model
      */
     public static function getUpdatedFields()
     {
-        return array();
+        return array("at" => self::translateKey("updatedTime"));
     }
 
 }
